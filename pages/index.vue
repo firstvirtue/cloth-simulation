709e5fbc263f92ac74f30eb1e8ff86f00245ff14
Jake Lee <jake@iropke.com>
2019-08-21 14:06:22 +0900

    Add threejs render module(진행중)

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import * as THREE from 'three';
 import AppLogo from '~/components/AppLogo.vue';
 import Spring from '~/assets/js/module/spring.js';
 import Point from '~/assets/js/module/point.js';
@@ -19,23 +20,28 @@ export default {
   },
   mounted() {
     // 변수 선언
-    const spacing = 20;
-    const clothX = 35;
-    const clothY = 35;
+    // const spacing = 20;
+    // const clothX = 35;
+    // const clothY = 35;
     const gravity = new Vector(0, 3.4);
     // const TIME = 0.016;
     const TIME = 0.05 * 0.5;
 
+    this.spacing = 20;
+    this.clothW = 25;
+    this.clothH = 25;
+
     let particles = [];
     let currentParticle;
+    const self = this;
 
     // 관계 생성
-    for (var y = 0; y < clothY; y++) {
-      for (var x = 0; x < clothX; x++) {
-        let point = new Point(x*spacing, y*spacing);
+    for (var y = 0; y < this.clothW; y++) {
+      for (var x = 0; x < this.clothH; x++) {
+        let point = new Point(x*this.spacing, y*this.spacing);
 
-        x!==0 && point.attach(particles[particles.length - 1], spacing); // X
-        y!==0 && point.attach(particles[(x) + (y-1) * (clothX)], spacing); // Y
+        x!==0 && point.attach(particles[particles.length - 1], this.spacing); // X
+        y!==0 && point.attach(particles[(x) + (y-1) * (this.clothW)], this.spacing); // Y
         particles.push(point);
       }
     }
@@ -53,9 +59,10 @@ export default {
     let canvas = document.getElementById('canvas');
     canvas.width = window.innerWidth - 20;
     canvas.height = window.innerHeight - 20;
+    // let ctx = canvas.getContext('2d');
+    // ctx.strokeStyle = '#555';
 
-    let ctx = canvas.getContext('2d');
-    ctx.strokeStyle = '#555';
+    this.init(canvas);
 
     let getClosestParticle = function(mouse) {
       let dist,
@@ -124,12 +131,26 @@ export default {
       });
 
       // 그리기
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      particles.forEach((particle) => {
-        particle.draw();
-      });
-      ctx.stroke();
+      // ctx.clearRect(0, 0, canvas.width, canvas.height);
+      // ctx.beginPath();
+      // particles.forEach((particle) => {
+      //   particle.draw();
+      // });
+      // ctx.stroke();
+
+      for (var i = 0; i < particles.length; i++) {
+        let v = particles[i].vPosition;
+
+        self.clothGeometry.vertices[i].x = v.x;
+        self.clothGeometry.vertices[i].y = v.y;
+      }
+
+      console.log(`particle: ${particles.length}, vertices: ${self.clothGeometry.vertices.length}`);
+
+      //
+      self.clothGeometry.verticesNeedUpdate  = true;
+      self.clothGeometry.computeVertexNormals();
+      self.renderer.render(self.scene, self.camera);
 
       // DEBUG
       //★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
@@ -141,6 +162,86 @@ export default {
       window.requestAnimFrame(update);
     })(0);
 
+  },
+  methods: {
+    init(canvas) {
+
+      // scene
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0xcce0ff);
+      this.scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
+
+      // camera
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+      this.camera.position.set(0, 0, 500);
+
+      // light
+      this.scene.add(new THREE.AmbientLight(0x666666));
+
+      let light = new THREE.DirectionalLight(0xdfebff, 1);
+      light.position.set(50, 200, 100);
+      light.position.multiplyScalar(1.3);
+      light.castShadow = true;
+      light.shadow.mapSize.width = 1024;
+      light.shadow.mapSize.height = 1024;
+
+      let d = 300;
+
+      light.shadow.camera.left = -d;
+      light.shadow.camera.right = d;
+      light.shadow.camera.top = d;
+      light.shadow.camera.bottom = -d;
+
+      light.shadow.camera.far = 1000;
+
+      this.scene.add(light);
+
+      var restDistance = 25;
+			var xSegs = 10;
+			var ySegs = 10;
+			var clothFunction = plane(restDistance * xSegs, restDistance * ySegs);
+      function plane(width, height) {
+				return function (u, v, target) {
+					var x = (u - 0.5) * width;
+					var y = (v + 0.5) * height;
+					var z = 0;
+					target.set(x, y, z);
+				};
+			}
+
+      var geometry = new THREE.BoxGeometry(1, 1, 1);
+      var cubematerial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      var cube = new THREE.Mesh(geometry, cubematerial);
+      this.scene.add(cube);
+
+      var loader = new THREE.TextureLoader();
+      var clothTexture = loader.load('textures/patterns/circuit_pattern.png');
+      clothTexture.anisotropy = 16;
+      var clothMaterial = new THREE.MeshLambertMaterial({
+        map: clothTexture,
+        side: THREE.DoubleSide,
+        alphaTest: 0.5
+      });
+
+      this.clothGeometry = new THREE.PlaneGeometry(this.clothW * this.spacing, this.clothH * this.spacing , this.clothW - 1, this.clothH - 1);
+      const material = new THREE.MeshBasicMaterial({ color: '#ff3030' });
+
+      // cloth mesh
+			this.object = new THREE.Mesh(this.clothGeometry, clothMaterial);
+      this.object.position.set( 0, 0, 0 );
+      this.object.castShadow = true;
+      this.scene.add(this.object);
+
+      // renderer
+      this.renderer = new THREE.WebGLRenderer( { antialias: true, canvas: canvas } );
+			this.renderer.setPixelRatio( window.devicePixelRatio );
+			this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+      // this.renderer.gammaInput = true;
+			// this.renderer.gammaOutput = true;
+			// this.renderer.shadowMap.enabled = true;
+
+    }
   }
 }
 
