9bdfecdec7f0f38e444642677e9d0e9fb0b5560b
Jake Lee <jake@iropke.com>
2019-08-29 19:17:27 +0900

    Add directional light

--- assets/js/module/render-engine.js
+++ assets/js/module/render-engine.js
@@ -20,49 +20,37 @@ class RenderEngine {
     this.camera.position.set(0, 0, 1000);
 
     // light
-    // [TODO] light를 수정하여 사실적인 질감을 줄 수 있음.
-    this.scene.add(new THREE.AmbientLight(0x666666));
-    let light = new THREE.DirectionalLight(0xdfebff, 1);
-    light.position.set(50, 200, 100);
-    light.position.multiplyScalar(1.3);
-    light.castShadow = true;
-    light.shadow.mapSize.width = 1024;
-    light.shadow.mapSize.height = 1024;
-
-    let d = 300;
-    light.shadow.camera.left = -d;
-    light.shadow.camera.right = d;
-    light.shadow.camera.top = d;
-    light.shadow.camera.bottom = -d;
-    light.shadow.camera.far = 1000;
-    this.scene.add(light);
+    this.setDirectionalLight();
+    // this.setPointLight();
 
     // 모델에 입힐 이미지 텍스쳐를 로드한다.
     // [TODO] Material을 수정하여 사실적인 질감을 줄 수 있음.
-    var loader = new THREE.TextureLoader();
-    var clothTexture = loader.load('textures/patterns/black-linen.png');
+    let loader = new THREE.TextureLoader();
+    let clothTexture = loader.load('textures/patterns/black-linen.png');
     clothTexture.anisotropy = 16;
-    var clothMaterial = new THREE.MeshLambertMaterial({
+    clothTexture.wrapS = THREE.RepeatWrapping;
+    clothTexture.wrapT = THREE.RepeatWrapping;
+    clothTexture.magFilter = THREE.NearestFilter;
+    let clothMaterial = new THREE.MeshPhongMaterial({
       map: clothTexture,
       side: THREE.DoubleSide,
-      alphaTest: 0.05,
-      opacity: 0.3,
       transparent: true
     });
 
     // 모델의 뼈대, 기하구조를 생성
     this.clothGeometry = new THREE.PlaneGeometry(cloth.clothW * cloth.spacing, cloth.clothH * cloth.spacing , cloth.clothW - 1, cloth.clothH - 1);
-    var material = new THREE.MeshBasicMaterial( {
-        // opacity: 0.85,
-        // transparent: true,
-        color: 0xdf0000
-      } );
 
     // 지오메트리와 머티리얼을 이용해 실제 그려질 메쉬를 생성하여 씬에 넣음
     this.object = new THREE.Mesh(this.clothGeometry, clothMaterial);
     this.object.position.set(0, 0, 0);
     this.object.castShadow = true;
     this.scene.add(this.object);
+
+    let geometry = new THREE.BoxBufferGeometry(100, 100, 100);
+    let material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 0, 10);
+    // this.scene.add(mesh);
 
     // 렌더러 선언
     this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
@@ -77,10 +65,40 @@ class RenderEngine {
     this.mouse = { x: 0, y: 0 };
   }
 
+  setDirectionalLight() {
+    this.light = new THREE.DirectionalLight(0xffffff, 1);
+    this.light.position.set(0, 200, 500);
+    this.light.target.position.set( 0, 0, 0);
+    this.scene.add(this.light);
+  }
+
+  setPointLight() {
+    let bulbGeometry = new THREE.SphereBufferGeometry( 14.02, 16, 8 );
+		let bulbLight = new THREE.PointLight( 0xffee88, 1, 100000000, 1 );
+		let bulbMat = new THREE.MeshStandardMaterial({
+			emissive: 0xffffee,
+			emissiveIntensity: 1,
+			color: 0x000000
+		});
+		bulbLight.add(new THREE.Mesh( bulbGeometry, bulbMat ));
+		bulbLight.position.set( 0, 200, 20 );
+		bulbLight.castShadow = true;
+
+    this.light = bulbLight;
+    this.light.position.set(10, 200, 100);
+    this.scene.add(this.light);
+  }
+
   render(particles) {
+
+    // light
+    // let time = Date.now() * 0.0005;
+		// this.light.position.y = Math.cos(time) * 100.75 + 1.25;
+
+    // cloth
     this.clothGeometry.verticesNeedUpdate  = true; //  vertex를 움직일 수 있는 플래그
     this.clothGeometry.computeVertexNormals();
-    for (var i = 0; i < particles.length; i++) {
+    for (let i = 0; i < particles.length; i++) {
       let v = particles[i].vPosition;
       this.clothGeometry.vertices[i].x = v.x;
       this.clothGeometry.vertices[i].y = v.y;
